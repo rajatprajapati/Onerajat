26bed3635a40ce64750c46dd1974bf4540faee6c
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-07-27 02:11:33 +0530

    navbar

---
+++ resources/js/scripts.js
@@ -0,0 +1,18 @@
+var links = document.getElementsByClassName('nav-link');
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+        var active = document.getElementsByClassName('active');
+        active[0].className = active[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+$(document).ready(function() {
+    $('#navbutton').click(function() {
+        if ($('#navbutton').hasClass('navhide')) {
+            $('#navbutton').removeClass('navhide');
+        } else {
+            $('#navbutton').addClass('navhide');
+        }
+    });
+});
