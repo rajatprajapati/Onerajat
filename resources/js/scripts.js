4098814b0ed91b4b4a12269bff7ea58feaf53827
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-07-28 15:11:00 +0530

    main page done

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -20,7 +20,17 @@ $(document).ready(function() {
             $('.navdisplay').click();
         });
     }
+    $('.full-height-width').addClass('zero-width');
 });
+window.onload = function() {
+    setTimeout(() => {
+        // document.getElementsByClassName('bodyanimate').classList.add('visible');
+        $('.bodyanimate').addClass('visible');
+    }, 1000);
+}
+
+window.onscroll = function() { hidefunction() }
+var prevScrollpos = window.pageYOffset;
 
 function hidefunction() {
     var currentScrollPos = window.pageYOffset;
@@ -30,8 +40,7 @@ function hidefunction() {
     } else {
         document.getElementById("header").style.top = "-100px";
         document.getElementById("header").style.transition = "0.5s";
+
     }
     prevScrollpos = currentScrollPos;
 }
-
-$('.fa').attr("aria-hidden", "false")
